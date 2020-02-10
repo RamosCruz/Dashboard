ef98f3b6d20ae22a7252e9c0d6fddd52946ebedf
Luis Enrique Ramos Cruz <lerc1991@gmail.com>
2020-02-10 08:43:01 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'm1n14b45t05';
+$config['encryption_key'] = 'm1n14b45t055';
 
 /*
 |--------------------------------------------------------------------------
